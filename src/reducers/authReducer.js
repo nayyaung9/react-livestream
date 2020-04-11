b396174fb65f02a1dde308fec2187186d5a79ff6
nayyaung9 <naygit.developer@gmail.com>
2020-04-11 15:55:44 +0630

    feat: added logged in user id

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,12 +2,13 @@ import { SIGN_IN, SIGN_OUT } from './types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 
 export function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload };
     case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
