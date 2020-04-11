f6a8c56645a33bd3392c7e9039ebec4123d09b3c
nayyaung9 <naygit.developer@gmail.com>
2020-04-11 13:58:40 +0630

    refactor: remove files and make route for pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route } from 'react-router-dom';
+
+// pages
+import StreamList from './pages/streams/StreamList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/streams" component={StreamList} />
+    </Router>
   );
 }
 
