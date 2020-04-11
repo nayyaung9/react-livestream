93d44b4edd62f75707b1d2d0bbd973f090452548
nayyaung9 <naygit.developer@gmail.com>
2020-04-11 13:59:46 +0630

    feat: create browser history for route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-
+import history from './history';
 // pages
 import StreamList from './pages/streams/StreamList';
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Route path="/streams" component={StreamList} />
     </Router>
   );
